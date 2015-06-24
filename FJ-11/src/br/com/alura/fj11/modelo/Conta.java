ef196012037500aa1861cb434cbcb7de077f5ccf
Victor <Victor@pc-Terry>
2015-06-23 21:57:18 -0300

    Concluindo exercicios 6,7,8,9,10 da aula "Pacote Java.lang"

--- FJ-11/src/br/com/alura/fj11/modelo/Conta.java
+++ FJ-11/src/br/com/alura/fj11/modelo/Conta.java
@@ -3,27 +3,95 @@ package br.com.alura.fj11.modelo;
 import br.com.alura.fj11.excecao.ValorInvalidoException;
 
 abstract public class Conta {
-
+	private int numero;
+	private String nome;
 	protected double saldo;
 
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * recupera o numero de uma conta
+	 * @return numero da conta
+	 */
+	public int getNumero() {
+		return numero;
+	}
+
+	/**
+	 * Altera o numero de uma conta
+	 * @param numero da conta
+	 */
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	/**
+	 * Esse método tem o objetivo de informar o total de dinheiro disponivel na
+	 * conta.
+	 * 
+	 * @ Saldo total da conta.
+	 */
 	public double getSaldo() {
 		return this.saldo;
 	}
 
+	/**
+	 * Esse método tem o objetivo de efetuar um saque em dinheiro na conta.
+	 * 
+	 * @param Valor a ser sacado
+	 *            
+	 */
 	void saca(double valor) {
 		this.saldo -= valor;
 	}
+
+	/**
+	 * Esse método tem o objetivo de depositar um valor em dinheiro na Conta.
+	 * 
+	 * @param Valor do deposito
+	 */
 
 	public void deposita(double deposito) {
 
 		if (deposito < 0) {
 			throw new ValorInvalidoException(
-					"NÃ£o Ã© permitido depositar valor negativo!");
+					"Não é  permitido depositar valor negativo!");
 		} else {
 			this.saldo += deposito;
 		}
 	}
-
+	
+	/**
+	 * Altera o retorno da mensagem toString ao recuperar um objeto Conta da memoria
+	 */
+	
+	public String toString() {
+		return "Esse objeto é uma conta com saldo R$ " + this.saldo;
+	}
+	
+	
+	public boolean equals(Object obj){
+		
+		Conta outraConta = (Conta) obj;
+		
+		if(this.numero == outraConta.numero && this.nome == outraConta.nome){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Atualiza a taxa referencial.
+	 * @param taxa
+	 */
 	public abstract void atualiza(double taxa);
 
 }
--- FJ-11/src/br/com/alura/fj11/programa/TesteInteger.java
+++ FJ-11/src/br/com/alura/fj11/programa/TesteInteger.java
@@ -1,16 +1,13 @@
 package br.com.alura.fj11.programa;
 
+import br.com.alura.fj11.modelo.Conta;
+import br.com.alura.fj11.modelo.ContaCorrente;
+
 public class TesteInteger {
-	public static void main (String [] args){
-		
-		Integer x1 = new Integer(10);
-		Integer x2 = new Integer(10);
-		
-		if (x1 == x2){
-			System.out.println("igual");
-		}
-		else{
-			System.out.println("diferente");
-		}
+
+	public static void main(String[] args) {
+		Conta conta = new ContaCorrente();
+		conta.deposita(1000);
+		System.out.println(conta);
 	}
 }
