1ebdb5e415da6bbfa55690600f05e369902acbd8
Victor <Victor@pc-Terry>
2015-06-25 21:34:24 -0300

    Lendo arquivo.txt

--- FJ-11/src/br/com/alura/fj11/programa/TesteJavaIO.java
+++ FJ-11/src/br/com/alura/fj11/programa/TesteJavaIO.java
@@ -1,26 +1,29 @@
 package br.com.alura.fj11.programa;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class TesteJavaIO {
-	public static void main(String [] args) throws IOException{
-		
+	public static void main(String[] args) throws IOException {
+
 		/**
-		 * Decorator Pattern:
-		 * 	Lendo arquivo utilizando APIs de mais baixo nivel a lógica é
-		 * 	Ler um byte, converter esse byte em char, e construir a String.
+		 * Decorator Pattern: Lendo arquivo utilizando APIs de mais baixo nivel
+		 * a lógica é Ler um byte, converter esse byte em char, e construir a
+		 * String.
 		 */
-		
-		InputStream in = System.in;
+		InputStream in = new FileInputStream(
+				"entrada.txt");
 		InputStreamReader isreader = new InputStreamReader(in);
 		BufferedReader bfreader = new BufferedReader(isreader);
-		
+
 		String s = bfreader.readLine();
 		System.out.println(s);
 
-		
+		bfreader.close();
+
 	}
+
 }
