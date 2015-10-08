ededfe165405124742538c854ccafa0865806568
vterry <victor.fsouza2@gmail.com>
2015-10-07 22:27:59 -0300

    Trabalhando com o Maven:
    - Gerando projetos e gerenciando dependencias.

---
+++ design-pattern/src/br/com/alura/strategy/exercicio/TestaRealizadorDeInvestimentos.java
@@ -0,0 +1,13 @@
+package br.com.alura.strategy.exercicio;
+
+public class TestaRealizadorDeInvestimentos {
+	public static void main(String[] args) {
+		
+		Conta c1 = new Conta();
+		RealizadorDeInvestimentos investimento = new RealizadorDeInvestimentos();
+
+		c1.deposita(1000.00);
+		investimento.realizaInvestimento(c1, new Arrojado());
+		System.out.println(c1.getSaldo());
+	}
+}
